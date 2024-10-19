5079c5376963b5c3b21c34c290f2b6a077f3bfe6
abhishekprajapati1 <ap0661236@gmail.com>
2024-10-19 10:33:10 +0530

    :fire: UPDATE: resolved merge conflicts

--- app/(protected)/placed-order/page.tsx
+++ app/(protected)/placed-order/page.tsx
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
 import DeletePlaceOrderFeature from "@/components/placed-order/deletePlaceOrderFeature";
 import EditPlaceOrderModel from "@/components/placed-order/EditPlaceOrderFeature";
 import Header from "@/components/placed-order/header";
 import HeaderNav from "@/components/placed-order/header-nav";
 import TableData from "@/components/placed-order/table_data";
 
-const page=()=>
-    {
-return(
+const page = () => {
+  return (
     <>
-   <Header />
-   <HeaderNav />
-   <TableData />
-        <EditPlaceOrderModel/>
-        <DeletePlaceOrderFeature />
+      <Header />
+      <HeaderNav />
+      <TableData />
+      <EditPlaceOrderModel />
+      <DeletePlaceOrderFeature />
     </>
-)
-}
-export default page;
-=======
-const Page = () => {
-  return <div></div>;
+  );
 };
-export default Page;
->>>>>>> ba83f601a81a3d09f3990d5ad02edfeaa50fed5a
+export default page;
