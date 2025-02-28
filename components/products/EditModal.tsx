aa26a52c281ec6f30e543f9d5c2838688bac0de7
prashant kumar <prashantdoa1996@gmail.com>
2025-02-28 12:25:33 +0530

    ⚡️ Extra commented code removed
    done

--- components/products/EditModal.tsx
+++ components/products/EditModal.tsx
@@ -1,17 +1,13 @@
 "use client";
-import { Button } from "@/components/ui/button";
+import React from "react";
 import {
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
 import Modal from "@/components/ui/modal";
-import React from "react";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { useAppDispatch, useAppSelector } from "@/store";
-import { editModal, setItemId } from "@/store/products/form.slice";
+import { editModal } from "@/store/products/form.slice";
 import ItemForm from "./ItemForm";
 
 const EditModal: React.FC = () => {
@@ -19,17 +15,12 @@ const EditModal: React.FC = () => {
   const modal = useAppSelector(
     (store) => store.productStore.formStore.editModal
   );
-  // const item_id = useAppSelector(
-  //   (store) => store.productStore.formStore?.item_id
-  // );
-
+ 
   return (
     <Modal
       showCloseIcon
       open={modal}
       onOpenChange={(val) => dispatch(editModal(val))}
-      // open={Boolean(item_id)}
-      // onOpenChange={(val) => dispatch(setItemId(val ? item_id : ""))}
       className="max-w-[45rem]"
     >
       <DialogHeader>
@@ -38,27 +29,6 @@ const EditModal: React.FC = () => {
           Make changes to product here. Click save when you're done.
         </DialogDescription>
       </DialogHeader>
-      {/* <div className="grid gap-4 py-4">
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="name" className="text-right">
-            Name
-          </Label>
-          <Input id="name" defaultValue="Pedro Duarte" className="col-span-3" />
-        </div>
-        <div className="grid grid-cols-4 items-center gap-4">
-          <Label htmlFor="username" className="text-right">
-            Username
-          </Label>
-          <Input
-            id="username"
-            defaultValue="@peduarte"
-            className="col-span-3"
-          />
-        </div>
-      </div> */}
-      {/* <DialogFooter>
-        <Button type="submit">Save changes</Button>
-      </DialogFooter> */}
       <ItemForm />
     </Modal>
   );
