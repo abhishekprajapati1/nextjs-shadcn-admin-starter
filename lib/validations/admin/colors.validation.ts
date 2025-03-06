54511a1618d400aa9b9ae261a2e8bcf88e2f555d
abhishekprajapati1 <ap0661236@gmail.com>
2025-03-06 16:39:57 +0530

    :sparkles: ENHANCEMENT: added name in color form

--- lib/validations/admin/colors.validation.ts
+++ lib/validations/admin/colors.validation.ts
@@ -2,5 +2,13 @@ import { PATTERNS } from "@/lib/constants";
 import { z } from "zod";
 
 export const formSchema = z.object({
-  color: z.string({ message: "Color must be a valid string." }).regex(PATTERNS.hex_color, { message: "Please provide a valid hex color code." }),
+  color: z
+    .string({ message: "Color must be a valid string." })
+    .regex(PATTERNS.hex_color, {
+      message: "Please provide a valid hex color code.",
+    }),
+  name: z
+    .string({ message: "Name must be a valid string." })
+    .min(2, { message: "Name must be at least 2 characters long." })
+    .max(100, { message: "Name must be at most 100 characters long." }),
 });
