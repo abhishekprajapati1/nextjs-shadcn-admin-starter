cc73fba98f3945f34a6094c8ecbb2dc2f324ea37
abhishekprajapati1 <ap0661236@gmail.com>
2025-03-13 13:40:00 +0530

    :fire: UPDATE: adding lens detail feature

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import ResetPasswordForms from "@/components/auth/reset-password/ResetPasswordForms";
 
 const ResetPasswordPage = () => {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-gradient-to-br from-background to-muted animate-smooth">
-      <ResetPasswordForms />
+      <React.Suspense>
+        <ResetPasswordForms />
+      </React.Suspense>
     </div>
   );
 };
